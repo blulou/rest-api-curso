7cc268b45bfe6e04a2fc9258f5c1b9e773fe4b9c
blulou <blulou97@gmail.com>
2017-12-18 21:50:22 -0600

    Creacion de los tokens
    create file "./controllers/auth.js"
    install module "moment"
    edit file "config.js"

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/shop'
+  db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+  SECRET_TOKEN: 'myclavedetokens'
 }
