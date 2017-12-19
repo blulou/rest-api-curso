9f1e1c7271be8850b89a7e08692364cb814f0a33
blulou <blulou97@gmail.com>
2017-12-18 19:53:42 -0600

    Nuevas funcionalidades
    Se ha añadido unas funciones que mejoran el rendimiento

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ mongoose.connect(config.db, { useMongoClient: true }, (err, res) => {
 
   app.listen(config.port, () => {
     console.log(`API REST corriendo en http://localhost:${config.port}`)
+    console.log('Nueva funcionalidad')
+    console.log('Otra nueva funcionalidad')
   })
 })
